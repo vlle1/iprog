caa135054e6a3a2d3ad3364485a1216b287ef7b4
vlle1 <36682087+vlle1@users.noreply.github.com>
2023-03-31 15:37:48 +0200

    Implement dummy model

---
+++ src/model/ActivitiesModel.js
@@ -0,0 +1,81 @@
+/**
+ * Activity json object "shape":
+ * { "activity": "some activity", "participants": numberOfPeople, "price": numberInEuros}
+ * 
+*/
+const SAVED_CHANGED = "savedActivitiesChanged";
+const REC_CHANGE = "recommendedActivitiesChanged";
+
+const DEFAULT_RECOMMENDED_ACTIVITIES = 5;
+
+class ActivityModel {
+  constructor() {
+    this.observers = []; //as in the lab.
+
+    this.recommendedActivities = []; //array of activity objects
+    this.savedActivities = []; //array of activity objects
+
+    this.filterText = "";
+    this.filterPriceLower = null; //could be number
+    this.filterPriceUpper = null; //could be number
+    this.filterPeople = null; //could be number (1, 2 or 3 etc.)
+
+    //we don't save the form entries for adding an activity
+  }
+
+  addSavedActivity(activity) {
+    this.savedActivities.push(activity);
+    this.notifyObservers(SAVED_CHANGED);
+  }
+  deleteSavedActivity(index) {
+
+    //remove index from savedActivities
+    this.savedActivities.splice(index, 1);
+    this.notifyObservers(SAVED_CHANGED);
+  }
+
+  getSavedActivities() {
+    return this.savedActivities;
+  }
+
+  getRecommendedActivities() {
+    return this.recommendedActivities;
+  }
+  regenerateRecommendedActivities() {
+    this.recommendedActivities = [];
+    this.fillUpRecommendedActivities(DEFAULT_RECOMMENDED_ACTIVITIES);
+  }
+  fillUpRecommendedActivities(targetAmount) {
+    let amountToGenerate = targetAmount - this.recommendedActivities.length;
+    for (let i = 0; i < amountToGenerate; i++) {
+      //TODO USE API LATER
+        this.recommendedActivities.push({"activity": "some activity", "participants": 1, "price": 0.5 * i});
+    }
+    this.notifyObservers(REC_CHANGE);
+  }
+
+  //observer:
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+  removeObserver(observer) {
+    function isNotTheObserverToRemoveCB(someObserver) {
+      return observer !== someObserver;
+    }
+    this.observers = this.observers.filter(isNotTheObserverToRemoveCB);
+  }
+  notifyObservers(message) {
+    //messages: "savedActivitiesChanged", "recommendedActivitiesChanged"
+    function invokeObserverCB(obs) {
+      try {
+        obs(message);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    this.observers.forEach(invokeObserverCB);
+  }
+  1;
+}
+
+export { ActivityModel, SAVED_CHANGED, REC_CHANGE }
