0a145ca771d6d032dcaa0d2733644df6b8368303
William Tomczak <wtomczak@kth.se>
2023-04-05 16:23:58 +0200

    syling sidebar WIP

--- src/views/sidebarView.jsx
+++ src/views/sidebarView.jsx
@@ -4,14 +4,24 @@ export default function sidebarView(props) {
         <div>
             <h2>Sidebar</h2>
            
-            <label for="priceFilter">Price:</label>
-            <input type="range" id="priceFilter" name="price" min ="0" max = "10000" value ="10000" > Price</input>
             
-            <label for="numOfResults">Number of results wanted:</label>
-            <input id="numOfResults"value=""> Number of results wanted</input>
             
-            <button onClick ={priceFilterCB} > Search</button>
+            <div>
+                <label htmlFor="peopleNr">Number of people</label>
+                <input id="peopleNr" value="" />
+            </div>
 
+            <div>
+                <label htmlFor="priceFilter">Price:</label>
+                <input type="range" id="priceFilter" name="price" min="0" max="10000" value="10000" />
+            </div>
+            <div >
+                <label for="numOfResults">Number of results wanted:</label>
+                <input id="numOfResults"value=""> Number of results wanted</input>
+            </div>
+            <div >
+                <button  onClick ={priceFilterCB} > Search</button>
+            </div>
         </div>
     )
 
